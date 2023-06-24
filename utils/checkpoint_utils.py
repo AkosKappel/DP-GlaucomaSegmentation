6b4307ef7a8b256bc6ad0efcd86cba31416f800a
Ákos Kappel <kappelakos@gmail.com>
2023-06-24 11:03:51 +0200

    add early stopping and checkpoints

---
+++ utils/checkpoint_utils.py
@@ -0,0 +1,18 @@
+import os
+import torch
+
+
+def save_checkpoint(state, filename='checkpoint.pth.tar', checkpoint_dir='../checkpoints'):
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    filename = os.path.join(checkpoint_dir, filename)
+    print(f'=> Saving checkpoint: {filename}')
+    torch.save(state, filename)
+
+
+def load_checkpoint(filename, model, optimizer, checkpoint_dir='../checkpoints'):
+    filename = os.path.join(checkpoint_dir, filename)
+    print(f'=> Loading checkpoint: {filename}')
+    checkpoint = torch.load(filename)
+    model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
