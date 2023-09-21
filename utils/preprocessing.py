179c0288babb399f02ba185b5f28836bec111a0c
Ákos Kappel <kappelakos@gmail.com>
2023-09-21 14:37:58 +0200

    new ROI method (combined Temp. matching and Thresh.)

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -4,13 +4,10 @@ from pathlib import Path
 
 __all__ = [
     'extract_optic_disc', 'extract_optic_cup',
-    'get_bounding_box', 'gaussian_kernel', 'circular_kernel', 'otsu',
-    'localize_roi',
-    'clahe', 'histogram_equalization',
-    'split_rgb_channels', 'to_greyscale',
-    'brightness_contrast', 'sharpen', 'blur',
-    'split_train_val_test',
-    'distance_transform', 'boundary_transform',
+    'get_bounding_box', 'gaussian_kernel', 'circular_kernel', 'parabolic_kernel', 'otsu',
+    'localize_roi', 'clahe', 'histogram_equalization',
+    'split_rgb_channels', 'to_greyscale', 'brightness_contrast', 'sharpen', 'blur',
+    'split_train_val_test', 'distance_transform', 'boundary_transform',
 ]
 
 
@@ -69,19 +66,21 @@ def get_bounding_box(binary_mask: np.ndarray) -> tuple[int, int, int, int]:
     return x, y, w, h
 
 
-def gaussian_kernel(width: int, height: int = None) -> np.ndarray:
+def gaussian_kernel(width: int, height: int = None, sigma: float = None) -> np.ndarray:
     if height is None:
         height = width
 
     shape = (height, width)
     size = np.max(shape)
     x, y = np.mgrid[-size:size + 1, -size:size + 1]
-    kernel = np.exp(-(x ** 2 + y ** 2) / (2 * size ** 2))
+
+    sigma = size / 2 if sigma is None else size / sigma
+    kernel = np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
 
     if shape != (size, size):
         kernel = cv.resize(kernel, shape[::-1], interpolation=cv.INTER_LINEAR)
 
-    return kernel / kernel.sum()
+    return kernel / kernel.max()
 
 
 def circular_kernel(width: int, height: int = None) -> np.ndarray:
@@ -95,6 +94,20 @@ def circular_kernel(width: int, height: int = None) -> np.ndarray:
     max_distance = np.sqrt(center_x ** 2 + center_y ** 2)
 
     kernel = 1 - (distances / max_distance)
+    return kernel
+
+
+def parabolic_kernel(width: int, height: int = None) -> np.ndarray:
+    if height is None:
+        height = width
+
+    center_x, center_y = width // 2, height // 2
+    y_coords, x_coords = np.ogrid[:height, :width]
+
+    distances = np.sqrt((x_coords - center_x) ** 2 + (y_coords - center_y) ** 2)
+    max_distance = np.sqrt(center_x ** 2 + center_y ** 2)
+
+    kernel = 1 - (distances / max_distance) ** 2
     return kernel
 
 
