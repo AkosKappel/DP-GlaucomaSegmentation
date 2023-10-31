fix scaling bug with ground truth in dataset class

ROI/neural_network/centernet.py
@@ -52,7 +52,7 @@ class Up(nn.Module):
 
 class CenterNet(nn.Module):
 
-    def __init__(self, n_classes: int = 1, scale: int = 1, base: str = 'resnet18', custom: bool = True, weights=None):
+    def __init__(self, n_classes: int = 1, scale: int = 4, base: str = 'resnet18', custom: bool = True, weights=None):
         super(CenterNet, self).__init__()
         self.scale = scale
 

ROI/neural_network/dataset.py
@@ -25,9 +25,6 @@ class CenterNetDataset(Dataset):
         image = cv.imread(image_id)
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-        original_image_size = max(image.shape[:2])  # Image should be square at this point
-        in_scale = original_image_size // self.input_size
-
         # Get bounding box and its label
         bboxes = target[['x', 'y', 'w', 'h']].values
         bboxes = bboxes.reshape(-1, 4)  # (n_boxes, 4)
@@ -40,7 +37,7 @@ class CenterNetDataset(Dataset):
             bboxes = np.array(augmented['bboxes'])
 
         # Create heatmap and regression map
-        heatmap, regmap = create_heatmap_and_regmap(bboxes, self.input_size, self.model_scale, in_scale)
+        heatmap, regmap = create_heatmap_and_regmap(bboxes, self.input_size, self.model_scale, 512 // self.input_size)
         return image, heatmap, regmap, bboxes, image_id, mask_id
 
 

ROI/neural_network/helpers.py
@@ -1,14 +1,18 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import torch
+from collections import defaultdict
 from pathlib import Path
 from tqdm.notebook import tqdm
 
 __all__ = [
-    'preprocess_centernet_input', 'detect_roi', 'prediction_to_bbox',
+    'preprocess_centernet_input', 'detect_roi', 'prediction_to_bbox', 'prediction_to_bboxes',
     'generate_centernet_dataset', 'generate_ground_truth_bbox_csv', 'generate_roi_dataset',
+    'pool_duplicates', 'merge_overlapping_boxes', 'calculate_iou', 'calculate_coverage', 'calculate_metrics',
+    'pad_to_square', 'resize_box_to_square',
 ]
 
 
@@ -219,7 +223,16 @@ def generate_roi_dataset(model, images: list[str], masks: list[str], dst_images_
             total_coverage += coverage
             pbar.set_postfix({'coverage': f'{total_coverage * 100 / i:.2f}%'})
             if coverage < 1.0:
-                print(f'Bounding box of {image_file} covers only {coverage * 100:.2f}% of the optic disc')
+                print(f'Bounding box of {image_file} covers {coverage * 100:.2f}% of the optic disc')
+                # Show the predicted bounding box on the whole image
+                mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
+                image[mask > 0] = 255
+                image[mask > 1] = 127
+                image = cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 5)
+                plt.figure(figsize=(8, 8))
+                plt.title(f'Coverage: {coverage * 100:.2f}%')
+                plt.imshow(image)
+                plt.show()
 
             # Visualize OD and OC overlay on the cropped and resized image
             roi_mask = np.repeat(roi_mask[:, :, np.newaxis], 3, axis=2)
@@ -438,6 +451,64 @@ def calculate_iou(box1, box2):
     intersection = (x2 - x1) * (y2 - y1)
     union = area_box1 + area_box2 - intersection
     return intersection / union
+
+
+def calculate_coverage(inner_box, outer_box):
+    inner_x1, inner_y1, inner_w, inner_h = inner_box
+    inner_x2, inner_y2 = inner_x1 + inner_w, inner_y1 + inner_h
+
+    outer_x1, outer_y1, outer_w, outer_h = outer_box
+    outer_x2, outer_y2 = outer_x1 + outer_w, outer_y1 + outer_h
+
+    # Calculate the area of the inner box
+    inner_area = inner_w * inner_h
+
+    # Inner box is completely outside of outer box
+    if inner_x1 >= outer_x2 or inner_x2 <= outer_x1 or inner_y1 >= outer_y2 or inner_y2 <= outer_y1:
+        return 0.0
+
+    # Calculate the intersection area
+    intersection_x1 = max(inner_x1, outer_x1)
+    intersection_x2 = min(inner_x2, outer_x2)
+    intersection_y1 = max(inner_y1, outer_y1)
+    intersection_y2 = min(inner_y2, outer_y2)
+    intersection_area = max(0, (intersection_x2 - intersection_x1) * (intersection_y2 - intersection_y1))
+
+    # Calculate the coverage percentage
+    coverage = intersection_area / inner_area
+    return coverage
+
+
+def calculate_metrics(pred_heatmaps, pred_regressions, true_bboxes, input_size, model_scale,
+                      threshold: float = 0.9, tolerance_margin: int = 0):
+    # Move to CPU and convert to numpy
+    pred_heatmaps = pred_heatmaps.detach().cpu().numpy()
+    pred_regressions = pred_regressions.detach().cpu().numpy()
+
+    results = defaultdict(list)
+    for i in range(pred_heatmaps.shape[0]):
+        # Get bounding boxes from heatmap and regression
+        pred_heatmap = pred_heatmaps[i].squeeze()
+        pred_regression = pred_regressions[i].squeeze()
+        true_bbox = true_bboxes[i].squeeze().numpy()
+
+        pred_bbox = prediction_to_bbox(pred_heatmap, pred_regression, input_size, model_scale, threshold)
+        pred_bbox = [
+            max(0, pred_bbox[0] - tolerance_margin),
+            max(0, pred_bbox[1] - tolerance_margin),
+            min(input_size, pred_bbox[2] + tolerance_margin),
+            min(input_size, pred_bbox[3] + tolerance_margin),
+        ]
+
+        # Calculate IoU
+        iou = calculate_iou(true_bbox, pred_bbox)
+        results['iou'].append(iou)
+
+        # Calculate coverage
+        coverage = calculate_coverage(true_bbox, pred_bbox)
+        results['coverage'].append(coverage)
+
+    return results
 
 
 def pad_to_square(img, mask, value: int = 0):

ROI/neural_network/loss.py
@@ -11,14 +11,14 @@ class CenterLoss(nn.Module):
         self.regr_weight = regr_weight
         self.size_average = size_average
 
-    def forward(self, prediction, mask, regr):
+    def forward(self, prediction, true_heatmap, true_regr):
         # Binary mask loss
-        pred_mask = torch.sigmoid(prediction[:, 0])
-        mask_loss = CenterLoss.focal_loss(pred_mask, mask)
+        pred_heatmap = torch.sigmoid(prediction[:, 0])
+        mask_loss = CenterLoss.focal_loss(pred_heatmap, true_heatmap)
 
         # Regression L1 loss
         pred_regr = prediction[:, 1:]
-        regr_loss = (torch.abs(pred_regr - regr).sum(1) * mask).sum(1).sum(1) / mask.sum(1).sum(1)
+        regr_loss = (torch.abs(pred_regr - true_regr).sum(1) * true_heatmap).sum(1).sum(1) / true_heatmap.sum(1).sum(1)
         regr_loss = regr_loss.mean(0)
 
         # Weighted total loss

ROI/neural_network/train.py
@@ -3,8 +3,6 @@ from collections import defaultdict
 from IPython.display import clear_output
 from tqdm.notebook import tqdm
 
-from .helpers import prediction_to_bbox
-
 
 def fit(model, optimizer, criterion, device, train_loader, val_loader, epochs,
         scheduler=None, early_stopping_patience: int = 10):
@@ -36,14 +34,12 @@ def fit(model, optimizer, criterion, device, train_loader, val_loader, epochs,
             preds = torch.cat((pred_heatmaps, pred_regressions), dim=1)
 
             # Compute loss
-            loss, mask_loss, regr_loss = criterion(preds, true_heatmaps, true_regressions)
+            loss, heatmap_loss, regression_loss = criterion(preds, true_heatmaps, true_regressions)
 
             # Update tracking variables in history
             running['train_loss'] += loss.item()
-            running['train_mask'] += mask_loss.item()
-            running['train_regr'] += regr_loss.item()
-
-            # TODO: add metrics for bounding box (e.g. IoU)
+            running['train_heatmap'] += heatmap_loss.item()
+            running['train_regression'] += regression_loss.item()
 
             # Backward pass
             loss.backward()
@@ -51,7 +47,7 @@ def fit(model, optimizer, criterion, device, train_loader, val_loader, epochs,
 
             # Show stats in progress bar
             pbar.set_description('Training: ' + ', '.join([
-                f'{k[5:]}: {v / batch_idx:.3f}' for k, v in running.items() if k.startswith('train')
+                f'{k[6:]}: {v / batch_idx:.3f}' for k, v in running.items() if k.startswith('train')
             ]))
 
         # Validation loop
@@ -70,16 +66,16 @@ def fit(model, optimizer, criterion, device, train_loader, val_loader, epochs,
                 preds = torch.cat((pred_heatmaps, pred_regressions), dim=1)
 
                 # Compute loss
-                loss, mask_loss, regr_loss = criterion(preds, true_heatmaps, true_regressions)
+                loss, heatmap_loss, regression_loss = criterion(preds, true_heatmaps, true_regressions)
 
                 # Update tracking variables in history
                 running['val_loss'] += loss.item()
-                running['val_mask'] += mask_loss.item()
-                running['val_regr'] += regr_loss.item()
+                running['val_heatmap'] += heatmap_loss.item()
+                running['val_regression'] += regression_loss.item()
 
                 # Show stats in progress bar
                 pbar.set_description('Validation: ' + ', '.join([
-                    f'{k[3:]}: {v / batch_idx:.3f}' for k, v in running.items() if k.startswith('val')
+                    f'{k[4:]}: {v / batch_idx:.3f}' for k, v in running.items() if k.startswith('val')
                 ]))
 
         # Save logs