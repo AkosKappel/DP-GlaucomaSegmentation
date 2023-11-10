3201eefad48fad5b72d8fc5af7fe93fc3fced36a
Ákos Kappel <kappelakos@gmail.com>
2023-11-10 15:18:51 +0100

    move extra networks to misc folder

---
+++ networks/misc/__init__.py
@@ -0,0 +1,30 @@
+# U-Net, U-Net++, U-Net 3+
+from .unet import *
+from .unetplusplus import *
+from .unet3plus import *
+
+# Residual, Recurrent, Recurrent Residual
+from .resunet import *
+from .runet import *
+from .r2unet import *
+
+# Attention
+from .attentionunet import *
+from .doubleattentionunet import *
+from .attentionsqueezeunet import *
+
+# Squeeze and Excitation
+from .squeezeunet import *
+from .sarunet import *
+from .usenet import *
+
+# Inception
+from .inceptionunet import *
+from .googleunet import *
+
+# Combined
+from .r2attentionunet import *
+from .r2unetplusplus import *
+
+# Vision Transformer
+from .transunet import *
